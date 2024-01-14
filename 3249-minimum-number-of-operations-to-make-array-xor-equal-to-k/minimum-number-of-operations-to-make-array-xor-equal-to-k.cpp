6527025ca8187a3c863adc8de0d31f561834a0be
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-01-14 15:49:20 +0530

    Time: 99 ms (97.72%) | Memory: 88.9 MB (38.73%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -3,16 +3,15 @@ public:
     int minOperations(vector<int>& nums, int k) {
         int xora= 0;
         for (int n : nums) {
-            xora ^= n;
+            xora^=n;
         }
         
         int res = 0;
-        k ^= xora;
-        while (k > 0) {
+        k^=xora;
+        while (k>0) {
             res += k&1;
             k >>= 1;
-        }
-        
+        }  
         return res;
     }
 };
