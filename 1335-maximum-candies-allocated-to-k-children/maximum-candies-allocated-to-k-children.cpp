15cccc08cbf4fee5b8ca5c202e1a39641b2dde1b
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-02-06 21:29:39 +0530

    Time: 155 ms (48.02%) | Memory: 86.7 MB (30.40%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -6,13 +6,12 @@ bool check(vector<int>&candies,int div,long long k)
     for(int x=0;x<candies.size();x++)
     {
         cnt+=(candies[x]/div);
-       
     }
     return cnt>=k;
 }
     int maximumCandies(vector<int>& candies, long long k) {
         int n=candies.size();
-        long long ans=0,diff=*max_element(candies.begin(),candies.end()),low=1,high=diff;
+        long long ans=0,diff=*max_element(candies.begin(),candies.end()),low=1,high=10000000000000;
         while(low<=high)
         {
             int mid=(low+high)/2;
