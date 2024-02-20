8fb0abc31d3be083fe044354b1122d8360b4a056
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-02-20 15:14:13 +0530

    Time: 11 ms (85.38%) | Memory: 20.4 MB (32.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int c=0;
-        sort(nums.begin(),nums.end());
-        for(int x=0;x<nums.size();x++)
-        {
-            if(c!=nums[x])
-            {
-                return c;
-            }
-            else{
-            c++;
-            }
-        }
-        return c;
+        // int c=0;
+        // sort(nums.begin(),nums.end());
+        // for(int x=0;x<nums.size();x++)
+        // {
+        //     if(c!=nums[x])
+        //     {
+        //         return c;
+        //     }
+        //     else{
+        //     c++;
+        //     }
+        // }
+        // return c;
+        int tot=accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size();
+        return (n*(n+1))/2-tot;
     }
 };
