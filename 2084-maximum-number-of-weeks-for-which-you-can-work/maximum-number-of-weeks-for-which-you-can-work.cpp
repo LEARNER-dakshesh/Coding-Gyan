d53e15d38ca9609cde843fb05747c171be9b7254
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-02-24 20:59:46 +0530

    Time: 97 ms (85.33%) | Memory: 77.9 MB (34.00%) - LeetSync

--- 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cpp
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long numberOfWeeks(vector<int>& milestones) {
-         int MOD = 1000000009 + 7;
+        int MOD = 1000000009 + 7;
         
         long long maxa = *max_element(milestones.begin(), milestones.end());
         long long tot = accumulate(milestones.begin(), milestones.end(), 0LL);
@@ -11,7 +11,7 @@ public:
             return tot;
         } else {
             long long ans = remaining * 2LL + 1LL;
-            return min(ans, tot);
+            return ans;
         }
     }
 };
