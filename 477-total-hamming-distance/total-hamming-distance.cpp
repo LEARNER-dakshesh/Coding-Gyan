ac8092e55d71a9ea6f7aecbb3dbac2fa6cf305f1
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-03-14 22:29:30 +0530

    Time: 33 ms (68.68%) | Memory: 21.3 MB (80.91%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int sum =0;
-        for(int x=0;x<nums.size();x++)
-        {
-            for(int y=0;y<nums.size();y++)
-            {
-               if(x!=y)
-               {
-                 int res=nums[x]^nums[y];
-                 int tot_setbit=__builtin_popcount(res);
-                //  cout<<res<<" "<<tot_setbit<<"      ";
-                sum+=tot_setbit;
-               }
+        // int sum =0;
+        // for(int x=0;x<nums.size();x++)
+        // {
+        //     for(int y=0;y<nums.size();y++)
+        //     {
+        //        if(x!=y)
+        //        {
+        //          int res=nums[x]^nums[y];
+        //          int tot_setbit=__builtin_popcount(res);
+        //         //  cout<<res<<" "<<tot_setbit<<"      ";
+        //         sum+=tot_setbit;
+        //        }
+        //     }
+        //     cout<<endl;
+        // }
+        // return sum/2;
+        int hammingdistance=0;
+        for(int bitpos=0;bitpos<32;bitpos++){
+        int setbits=0 ;
+            for(int i = 0 ;i<nums.size();i++){
+                setbits+=(nums[i]>>bitpos)&1;
+                
             }
-            cout<<endl;
+            hammingdistance+=setbits * (nums.size()-setbits);
         }
-        return sum/2;
+        return hammingdistance;
     }
+
 };
