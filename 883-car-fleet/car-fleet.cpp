866e66293c47287cd3e52244184f2ec4855d1289
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-05-04 02:33:13 +0530

    Time: 420 ms (5.01%) | Memory: 99.6 MB (6.93%) - LeetSync

---
+++ 883-car-fleet/car-fleet.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<double>time;
+        for(int x=0;x<position.size();x++)
+        {
+           double dis=target-position[x];
+           double state = static_cast<double>(dis) / speed[x];
+           time.push_back(state);
+        }
+       vector<pair<int, double>> p;
+        for(int x=0;x<position.size();x++)
+        {
+            p.push_back(make_pair(position[x], time[x]));
+        }
+
+        sort(p.rbegin(),p.rend());
+        double t1=p[0].second;
+        int count=1;
+        for(int x=0;x<p.size();x++)
+        {
+            cout<<p[x].first<<" "<<p[x].second<<endl;
+        }
+        cout<<endl;
+        for(auto it=p.begin()+1;it!=p.end();it++)
+        {
+            double t2=it->second;
+            if(t2>t1)
+            {
+                t1=t2;
+                count+=1;
+            }
+        }
+        return count;
+    }
+};
