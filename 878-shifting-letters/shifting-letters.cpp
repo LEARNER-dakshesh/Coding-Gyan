50b28fa846680a917b8719ace19e2f548f02bcc7
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-05-19 16:41:28 +0530

    Time: 98 ms (61.73%) | Memory: 75.6 MB (17.33%) - LeetSync

---
+++ 878-shifting-letters/shifting-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        // int total=accumulate(shifts.begin(),shifts.end(),0);
+             int n=shifts.size();
+        reverse(shifts.begin(),shifts.end());
+        for(int x=0;x<shifts.size();x++)
+        {
+            shifts[x]=shifts[x]%26;
+        }
+        vector<long long>prefix(shifts.size(),0);
+        prefix[0]=shifts[0];
+         for(int x=1;x<shifts.size();x++)
+         {
+            prefix[x]=prefix[x-1]+shifts[x];
+         }
+        reverse(prefix.begin(),prefix.end());
+        for(int x=0;x<prefix.size();x++)
+        {
+            prefix[x]=prefix[x]%26;
+            int dist=123-s[x];
+            if(prefix[x]<dist)
+            {
+            s[x]=s[x]+prefix[x];
+            }
+            else{
+                prefix[x]-=dist;
+                s[x]='a'+prefix[x];
+            }
+        }
+        return s;
+
+    }
+};
