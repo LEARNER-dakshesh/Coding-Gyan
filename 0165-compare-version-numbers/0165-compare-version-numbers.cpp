6baeb4d3ad4ea838f4de73aff6770ca3f60089fa
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-09-15 15:57:24 +0530

    Time: 3 ms (42.8%), Space: 8.3 MB (21.1%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+vector<int> splitVersion(const string& version) {
+    vector<int> result;
+    stringstream ss(version);
+    string part;
+    
+    while (getline(ss, part, '.')) {
+        result.push_back(stoi(part)); 
+    }
+    
+    return result;
+}
+
+int compareVersion(string version1, string version2) {
+    vector<int> v1 = splitVersion(version1);
+    vector<int> v2 = splitVersion(version2);
+    
+    int len = max(v1.size(), v2.size());
+    for (int i = 0; i < len; ++i) {
+        int num1 = i < v1.size() ? v1[i] : 0; 
+        int num2 = i < v2.size() ? v2[i] : 0;
+        
+        if (num1 < num2) {
+            return -1;
+        } else if (num1 > num2) {
+            return 1;
+        }
+    }
+    
+    return 0;
+}
+};
