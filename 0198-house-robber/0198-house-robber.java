7630b8eb5c409a32ebb25371ec7635a5c6d9bec9
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-10-20 05:04:22 +0530

    Time: 0 ms (100%), Space: 41.1 MB (26.02%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+      int even = 0;int odd=0;
+      for(int i =0;i<nums.length;i++){
+          if(i%2==0){
+              even = Math.max(even+nums[i],odd);
+          }
+          else{
+              odd = Math.max(odd+nums[i],even);
+          }
+      } 
+      if(even>odd){
+          return even;
+      } 
+      else return odd;
+    }
+}
