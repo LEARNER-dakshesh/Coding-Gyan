f4309173542bddb985f0866f6b72926ac80e6c27
Dakshesh Gupta <96490901+LEARNER-dakshesh@users.noreply.github.com>
2024-11-14 23:52:12 +0530

    Time: 79 ms (19.85%), Space: 94.7 MB (13.36%) - LeetHub

---
+++ 2655-rearrange-array-to-maximize-prefix-score/2655-rearrange-array-to-maximize-prefix-score.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+       int n=nums.size();
+       sort(nums.rbegin(),nums.rend());
+       int sum=0;
+ 
+        vector<long long>pre(n);
+        pre[0]=nums[0];
+       for(int x=1;x<nums.size();x++)
+       {
+          pre[x]=pre[x-1]+nums[x];
+       }
+
+       int ans=0;
+       for(int x=0;x<n;x++)
+       {
+          if(pre[x]>0LL)
+          {
+            ans++;
+          }
+       }
+       return ans;
+    }
+};
